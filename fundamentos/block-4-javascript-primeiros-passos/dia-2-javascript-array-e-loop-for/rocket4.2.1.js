60d66bf144b393b091d5e9fb9cdeeca609a2867a
Licínio Pereira dos Santos Junior <110546077+liciniopsj@users.noreply.github.com>
2022-08-19 18:19:10 -0300

    Update rocket4.2.1.js
    cleaning code

--- fundamentos/block-4-javascript-primeiros-passos/dia-2-javascript-array-e-loop-for/rocket4.2.1.js
+++ fundamentos/block-4-javascript-primeiros-passos/dia-2-javascript-array-e-loop-for/rocket4.2.1.js
@@ -1,15 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let bigger = 0;
+let highest = 0;
 
-for (let i = 0; i < numbers.length - 1; i += 1) {
-    if (numbers[i] > numbers[i + 1]) {
-        if (numbers[i] > bigger) {
-            bigger = numbers[i];
-        }
-    } else {
-        if (numbers[i + 1] > bigger) {
-        }
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > highest) {
+        highest = numbers[i];
     }
 }
 
-console.log('O maior numero do array é: ', bigger);
+console.log(highest);
