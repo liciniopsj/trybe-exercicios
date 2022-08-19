ef68983b542bb7175e0176c1d35dc8e11548a7d4
liciniopsj <licinio.psj@gmail.com>
2022-08-19 17:29:36 -0300

    :rocket: 4.2 - finding and showing the highest number in the array

---
+++ fundamentos/block-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/rocket4.2.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let bigger = 0;
+
+for (let i = 0; i < numbers.length - 1; i += 1) {
+    if (numbers[i] > numbers[i + 1]) {
+        if (numbers[i] > bigger) {
+            bigger = numbers[i];
+        }
+    } else {
+        if (numbers[i + 1] > bigger) {
+        }
+    }
+}
+
+console.log(bigger);
