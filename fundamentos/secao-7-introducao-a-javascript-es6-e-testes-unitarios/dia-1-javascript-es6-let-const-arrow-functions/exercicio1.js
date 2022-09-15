fc9a946753fb3280b94ef838a8eaa5cbe7489e84
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-15 18:41:23 -0300

    part 1, assignment 1

---
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions/exercicio1.js
@@ -0,0 +1,13 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    console.log(elseScope);
+  }
+ 
+}
+
+testingScope(true);
