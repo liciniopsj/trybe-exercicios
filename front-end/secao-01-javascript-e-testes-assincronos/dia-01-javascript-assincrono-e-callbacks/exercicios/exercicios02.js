c8429687ec558658943f209bdcf46a390bf1197e
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-06 19:13:00 -0300

    Exercicios 1, 2, 3

---
+++ front-end/secao-01-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicios02.js
@@ -0,0 +1,34 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} está a ${value} ${measurementUnit} de distância do Sol`;
+
+const MEASUREMENT_UNIT = 'quilômetros';
+
+const mars = {
+  name: 'Marte',
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const venus = {
+  name: 'Venus',
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const jupiter = {
+  name: 'Jupiter',
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+//Resposta A, C, B
