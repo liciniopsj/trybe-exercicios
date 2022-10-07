55f9da99007e6d4a9f74325ae51cce1efb2f9802
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-07 16:11:04 -0300

    fetch.js

--- front-end/secao-01-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-wait/exemplos02/fetch.js
+++ front-end/secao-01-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-wait/exemplos02/fetch.js
@@ -3,7 +3,13 @@ const fetch = require('node-fetch');
 const fetchJoke = () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  console.log(fetch(url));
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
 }
 
 fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
