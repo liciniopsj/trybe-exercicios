0c31ea6b8a3db3f9f0e502884bdce8a2c124e204
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-07 16:13:45 -0300

    async.js

---
+++ front-end/secao-01-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-wait/exemplos02/async.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.value)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+  
+  console.log(result);
+};
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
