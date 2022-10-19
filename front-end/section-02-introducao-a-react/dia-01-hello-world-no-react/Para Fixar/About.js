7f808a2b53ca317cf8b3175faa03ffd4ca68f9c6
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-19 18:44:28 -0300

    S02-D01 | Hello World in React | src files

---
+++ front-end/section-02-introducao-a-react/dia-01-hello-world-no-react/Para Fixar/About.js
@@ -0,0 +1,16 @@
+
+function About() {
+  return <>
+    <h1>Licinio Pereira dos Santos Junior</h1>
+    <p>Aluno do Curso de Web Developer da Trybe</p>
+    <h2>Minhas Habilidades</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>Jest</li>
+    </ul>
+  </>
+}
+
+export default About;
---
+++ front-end/section-02-introducao-a-react/dia-01-hello-world-no-react/Para Fixar/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import About from './About';
+
+function App() {
+return <About/>
+}
+
+export default App;
