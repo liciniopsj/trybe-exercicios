34c2393671f6484df4a10b0c71914198c1424d1a
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-19 20:18:15 -0300

    src files from sd-026-a-exercise-frontend-content

---
+++ front-end/section-02-introducao-a-react/dia-01-hello-world-no-react/sd-026-a-exercise-frontend-content/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <h1>Exercise - Frontend content</h1>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/section-02-introducao-a-react/dia-01-hello-world-no-react/sd-026-a-exercise-frontend-content/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>Conteúdos de Frontend</h1>
+    );
+  }
+}
+
+export default Header;
