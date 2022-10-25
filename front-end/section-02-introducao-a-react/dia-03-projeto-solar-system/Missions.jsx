22f4ce8015d0288f382929a90c844281900759d0
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-25 18:07:17 -0300

    added Solar system component files

---
+++ front-end/section-02-introducao-a-react/dia-03-projeto-solar-system/Missions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((mission, index) => (
+          <MissionCard
+            key={ index }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Missions;
---
+++ front-end/section-02-introducao-a-react/dia-03-projeto-solar-system/SolarSystem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {planets.map((planet, index) => (
+          <PlanetCard
+            key={ index }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
